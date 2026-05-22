a0ea7b4c87c484ef08892aa7bee48cff36a43eb9
Abijith Suresh <58354654+abijith-suresh@users.noreply.github.com>
2026-05-22 15:55:53 +0530

    chore: remove unused format option (#131)

--- src/components/app/controls/FormatSelect.tsx
+++ src/components/app/controls/FormatSelect.tsx
@@ -2,15 +2,13 @@ import { useImageApp } from "@/components/app/state/ImageAppContext";
 import Select from "@/components/ui/Select";
 import { FORMAT_OPTIONS } from "@/config/selectOptions";
 
-const PROCESS_FORMAT_OPTIONS = FORMAT_OPTIONS.filter((option) => option.value !== "");
-
 export default function FormatSelect() {
   const { state, actions } = useImageApp();
 
   return (
     <Select
       id="format-select"
-      options={PROCESS_FORMAT_OPTIONS}
+      options={FORMAT_OPTIONS}
       value={state.formatValue()}
       onChange={actions.setFormatValue}
       disabled={!state.controlsActive() || state.formatSelectDisabled()}
