6e01df5d0d19bce6238fcfa3ec01c36527431878
Abijith Suresh <58354654+abijith-suresh@users.noreply.github.com>
2026-05-22 15:24:02 +0530

    chore: remove unused public surface (#130)

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -5,16 +5,6 @@
 
 import { ROUTES, GITHUB_URL } from "./constants";
 
-export interface NavLink {
-  label: string;
-  href: string;
-}
-
-export interface FooterSection {
-  title: string;
-  links: NavLink[];
-}
-
 export const navigation = {
   header: {
     links: [
